4e6259aef12cad50d2d5c7dc58b4e6e16e0d16bd
thetansabe <93122964+thetansabe@users.noreply.github.com>
2025-08-10 16:56:20 +0700

    Time: 0 ms (100.00%) | Memory: 31.2 MB (61.54%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cs
+++ 900-reordered-power-of-2/reordered-power-of-2.cs
@@ -1,32 +1,7 @@
-// public class Solution {
-//     public bool ReorderedPowerOf2(int n) {
-//         Dictionary<int, int> freq = new(); // { digit : count}
-//         int numLen = 0;
-
-//         // 1. init freq map
-//         while(n > 0){
-//             int digit = n % 10;
-//             if(!freq.ContainsKey(digit)){
-//                 freq[digit] = 0;
-//             }
-//             freq[digit] += 1;
-//             n = n / 10;
-//             ++numLen;
-//         }
-
-//         // 2. generate permutations
-//         for(int i = 0; i < numLen; i++){
-//             // code to gen permuatations from map 
-//         }
-//     }
-// }
-
 public class Solution {
-public bool ReorderedPowerOf2(int n) {
-    // Helper function to count digit frequencies of a number.
-    // It returns an integer array of size 10 where the index represents the digit
-    // and the value represents its frequency.
-    int[] CountDigits(int num) {
+    // { index = digit, value = count }
+    // each iteration will reduce num / 10 => log10(N) ~ O(logN)
+    public int[] CountDigits(int num) {
         int[] freq = new int[10];
         while (num > 0) {
             freq[num % 10]++;
@@ -35,22 +10,35 @@ public bool ReorderedPowerOf2(int n) {
         return freq;
     }
 
-    // 1. Get the digit frequencies of the input number n.
-    int[] nFreq = CountDigits(n);
+    // DO NOT need to iterate through permutation set from n.
+    // n with 10^9 digits: is like 9876543210 repeat 9 times
+    // we will have to check (9*9*8*...*1)*9 = 9*9!*9 ~ 3.10^7
+    // => this still works but don't do this..
 
-    // 2. Iterate through all powers of 2.
+    // We should iterate through all powers of 2.
     // The maximum possible value for n is 10^9, so the largest power of 2
     // we need to check is 2^29 (which is less than 10^9).
-    // The length of the number n and the power of 2 must be the same.
-    for (int i = 0; i < 30; i++) {
-        int powerOf2 = 1 << i; // This is equivalent to 2^i.
+    // WHY 29? just find largest x that log10(2^x) < log10(10^9)
+
+    // Overall TC: O(logN) ~ O(log10(10^9)) = 9 ~ O(1)
+    public bool ReorderedPowerOf2(int n) {
+        // 1. Get the digit frequencies of the input number n.
+        int[] nFreq = CountDigits(n);
         
-        // 3. Get the digit frequencies of the current power of 2.
-        int[] powerFreq = CountDigits(powerOf2);
-        
-        // 4. Compare the digit frequencies.
-        // If the frequencies match, we've found a valid reordering.
-        if (nFreq.Length == powerFreq.Length) {
+        // 2. Iterate through 29 power of i
+        // The length of the number n and the power of 2 must be the same.
+        for (int i = 0; i < 30; i++) {
+            int powerOf2 = 1 << i; // This is equivalent to 2^i.
+            
+            // 3. Get the digit frequencies of the current power of 2.
+            int[] powerFreq = CountDigits(powerOf2);
+            
+            // 4. Compare the digit frequencies.
+            // If the frequencies match, we've found a valid reordering.
+            if (nFreq.Length != powerFreq.Length) {
+                continue;
+            }
+
             bool match = true;
             for (int j = 0; j < 10; j++) {
                 if (nFreq[j] != powerFreq[j]) {
@@ -62,9 +50,8 @@ public bool ReorderedPowerOf2(int n) {
                 return true;
             }
         }
-    }
 
-    // If no power of 2 has matching digit frequencies, return false.
-    return false;
-}
+        // If no power of 2 has matching digit frequencies, return false.
+        return false;
+    }
 }
